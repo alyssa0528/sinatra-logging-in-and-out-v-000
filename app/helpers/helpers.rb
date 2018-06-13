397347466b5084b4c7ab790f82a3905623365328
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-13 09:38:24 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class Helpers
 
-  #def User.current_user(session)
-  #  @user = User.find_by(session[:user_id])
-  #end
+  def self.current_user(session)
+    @user = User.find_by(session[:user_id])
+  end
 
-  #def self.is_logged_in?(session)
+  def self.is_logged_in?(session)
 
-  #end
+  end
 end
